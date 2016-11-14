000b1e32e874de83a6fbaa3394b03da1b48ae802
Kyle Rico <Kyle Rico>
2016-11-14 11:35:05 -0800

    adding everything

---
+++ routes/done.js
@@ -0,0 +1,27 @@
+exports.deleteTask = function(req, res) {
+	var newTask = {
+
+			"idx" : req.query.idx,
+
+			"task": req.query.task,
+
+			"subject": req.query.subject,
+
+			"dueDate": req.query.dueDate,
+		};
+
+
+if(req.query.task){
+
+	var tasks = req.app.get('taskData').cogs;
+	tasks.splice(req.query.idx, 1);
+	req.app.get('taskData').cogs = tasks;
+	
+	req.app.get('taskData').finished.push(newTask);
+
+};
+
+
+ 	// res.render('tasks', data);
+ res.redirect('/tasks');
+};
