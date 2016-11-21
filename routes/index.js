56260b70d8c0e52fa43a7aa56bdb8b41d61e9cca
Ashok Kunda <Ashok Kunda>
2016-11-21 13:01:03 -0800

    tasks on home page are only those due today

--- routes/index.js
+++ routes/index.js
@@ -18,13 +18,24 @@ feed.posts.forEach(function(post) {
 data.indexData.tryToBeat = tryToBeat;
 
 exports.view = function(req, res) {
-    var toDo = req.app.get('taskData').cogs.length;
+
+    var tasks = req.app.get('taskData').cogs;
+    
+    var toDo = tasks.length;
     var fin = req.app.get('taskData').lifecount.length;
     var prog = 100*(fin/(toDo+fin));
     var active = "active";
 
+    var dueToday = [];
+    var utc = new Date().toJSON().slice(0,10);
+    var today = utc.substr(5, 10).concat("-").concat(utc.substr(0, 4));
+    tasks.forEach ( function (task) {
+        if (task.dueDate == today) {
+            dueToday.push(task);
+        }
+    });
 
+    data.indexData.dueToday = dueToday;
 	data.indexData.prog = Math.round(prog);
 	res.render('index', data.indexData);
-
 };
