ab5032bdb14e006bf0de4dbe9ee01f612c3ad782
Ashok Kunda <Ashok Kunda>
2016-11-21 12:07:55 -0800

    done button now works properly

--- routes/done.js
+++ routes/done.js
@@ -1,28 +1,30 @@
 exports.deleteTask = function(req, res) {
+
 	var newTask = {
+		"idx" : req.query.idx,
+		"task": req.query.task,
+		"subject": req.query.subject,
+		"dueDate": req.query.dueDate,
+		"description": req.query.description
+	};
 
-			"idx" : req.query.idx,
+	if(req.query.task){
 
-			"task": req.query.task,
+		var tasks = req.app.get('taskData').cogs;
+		tasks.splice(req.query.idx, 1);
 
-			"subject": req.query.subject,
+		tasks.forEach (function (task) {
+			if (task.idx > req.query.idx) {
+				task.idx -= 1;
+			}
+		});
 
-			"dueDate": req.query.dueDate,
-		};
+		req.app.get('taskData').cogs = tasks;
 
-
-if(req.query.task){
-
-	var tasks = req.app.get('taskData').cogs;
-	tasks.splice(req.query.idx, 1);
-	req.app.get('taskData').cogs = tasks;
-	
-	req.app.get('taskData').finished.push(newTask);
-	req.app.get('taskData').lifecount.push(newTask);
-
-};
-
+		req.app.get('taskData').finished.push(newTask);
+		req.app.get('taskData').lifecount.push(newTask);
+	};
 
  	// res.render('tasks', data);
- res.redirect('/tasks');
+ 	res.redirect('/tasks');
 };
