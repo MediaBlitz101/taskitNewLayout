a39f4acef8d5c5b9f28f8671b6467ea7115d576f
Nayeli Ruiz <Ruiz.Nayeli09@gmail.com>
2016-11-22 11:23:48 -0800

    style all pages

--- routes/feed.js
+++ routes/feed.js
@@ -6,21 +6,6 @@ var everyone = {
 	"unknown": others
 }
 
-
-var tryToBeat = { score: Number.MAX_SAFE_INTEGER };
-
-// Write the callback function
-function handleFile(err, data) {
-    if (err) throw err
-    obj = JSON.parse(data)
-        // You can now play with your datas
-}
-
-feed.posts.forEach(function(post) {
-    if (post.score > data.indexData.myScore && tryToBeat.score > post.score)
-        tryToBeat = post;
-});
-
 exports.view = function(req, res){
 	res.render('feed', everyone);
 };
