ccda0a7a98cecf8940869cc40781ba7bd522410e
Ashok Kunda <Ashok Kunda>
2016-11-28 14:49:47 -0800

    added friend req button on non friends pages

--- routes/profile.js
+++ routes/profile.js
@@ -7,14 +7,27 @@ exports.personInfo = function(req, res) {
 	
 	var name = req.params.username;
 	var person;
+	var isFriend = false;
 
-	for (var i = 0; i < people.length; i++) {
-		console.log(people[i].username);
-		if (people[i].username == req.params.name) {
+	for (var i = 0; i < data.length; i++) {
+		console.log(data[i].username);
+		if (data[i].username == req.params.name) {
 			// console.log(i, "\t", data[i]);
-			person = people[i];
+			person = data[i];
+			isFriend = true;
 		}
 	}
 
+	if (!isFriend) {
+		for (var i = 0; i < others.length; i++) {
+			console.log(others[i].username);
+			if (others[i].username == req.params.name) {
+				// console.log(i, "\t", others[i]);
+				person = others[i];
+			}
+		}
+	}
+
+	person.isFriend = isFriend;
 	res.render('profile', person);
 }
